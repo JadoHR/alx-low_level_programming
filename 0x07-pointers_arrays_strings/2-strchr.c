1c152830ed39357457cfbcc22125ebedc16b1515
JadoHR <jeandedieuhab13@gmail.com>
2023-04-03 07:33:45 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * _strchr - Entry point
  * @s:input
@@ -10,9 +11,10 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[i] == )
+		if (s[i] == c)
 			return (s + i);
 	}
 
